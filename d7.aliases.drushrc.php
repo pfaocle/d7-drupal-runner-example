0c32458796ae7ad9d82815a7fed28a57a7e9a50c
Paul Byrne <paul.byrne@ixis.co.uk>
2014-02-03 22:21:36 +0000

    Add an example Drush alias file.

---
+++ d7.aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Example alias for testing Drupal Runner.
+ */
+
+$aliases['local'] = array(
+  'uri' => 'd7.drupal.dev:8080',
+  'root' => '/webroot/d7.drupal.dev',
+  'remote-host' => '33.33.33.10',
+  'remote-user' => 'vagrant',
+);
